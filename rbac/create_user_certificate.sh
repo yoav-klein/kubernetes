07f827fb17ba12606a5bb3dec11d6e69b67136c1
yoavklein <yoavklein25@gmail.com>
2021-05-31 21:41:10 +0000

    RBAC Demonstration

---
+++ rbac/create_user_certificate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# creating a key
+openssl genrsa -out yoav.key
+
+# NOTE: the CN must correspond to the name in the RoleBinding
+openssl req -new -key yoav.key -out yoav.csr \
+  -subj "/CN=yoav"
+
+# sign the csr with the cluster's CA
+sudo openssl x509 -req -in yoav.csr -CA /etc/kubernetes/pki/ca.crt \
+  -CAkey /etc/kubernetes/pki/ca.key \
+  -CAcreateserial \
+  -out yoav.crt -days 365
+
