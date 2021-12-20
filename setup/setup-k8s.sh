d824d84abc2e5ab1fa43053b88fa30dd020518bc
yoav <yoavklein25@gmail.com>
2021-12-20 17:27:20 +0000

    Fixing setup-k8s.sh

--- setup/setup-k8s.sh
+++ setup/setup-k8s.sh
@@ -1,7 +1,8 @@
 
+set -e
 install_common_packages()
 {
-	apt-get update && apt-get install -y curl apt-transport-https \
+	sudo apt-get update && sudo apt-get install -y curl apt-transport-https \
 		ca-certificates gnupg lsb-release
 
 }
@@ -9,18 +10,18 @@ install_common_packages()
 containerd_config()
 {
 	# load these kernel modules on boot
-	echo "br_netfilter" >> /etc/module-load.d/containerd.conf
-	echo "overlay" >> /etc/module-load.d/containerd.conf
+	echo "br_netfilter" | sudo tee /etc/modules-load.d/containerd.conf
+	echo "overlay" | sudo tee /etc/modules-load.d/containerd.conf
 	# load them now
-	modprobe overlay
-	modprobe br_netfilter
+	sudo modprobe overlay
+	sudo modprobe br_netfilter
 
 	# set some system configurations for containerd
-	echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.d/99-kubernetes-cri.conf
-	echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/99-kubernetes-cri.conf
-	echo "net.bridge.bridge-nf-call-ip6tables = 1" >> /etc/sysctl.d/99-kubernetes-cri.conf
+	echo "net.bridge.bridge-nf-call-iptables = 1" | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+	echo "net.ipv4.ip_forward = 1" | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+	echo "net.bridge.bridge-nf-call-ip6tables = 1" | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 
-	sysctl --system
+	sudo sysctl --system
 }
 
 docker_install()
@@ -28,17 +29,17 @@ docker_install()
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" >> /etc/apt/sources.list.d/docker.list
-	apt-get update
-	apt-get install docker-ce docker-ce-cli containerd.io
+	sudo apt-get update
+	sudo apt-get install docker-ce docker-ce-cli containerd.io
 }
 
 containerd_install()
 {
 	containerd_config
 
-	apt-get update && apt-get install -y containerd
-	mkdir -p /etc/containerd
-	containerd config default | tee /etc/containerd/config.toml
+	sudo apt-get update && sudo apt-get install -y containerd
+	sudo mkdir -p /etc/containerd
+	containerd config default | sudo  tee /etc/containerd/config.toml
 	systemctl restart containerd
 }
 
@@ -51,6 +52,6 @@ k8s_install()
 
 }
 
-install_common_packages
+
 
 
