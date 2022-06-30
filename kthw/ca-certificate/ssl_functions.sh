892697551e729ec08119b760c4d4a2b847308d3f
yoav <yoavklein25@gmail.com>
2022-06-30 16:39:42 +0000

    PKI: generate client certificates for kubelets

--- kthw/ca-certificate/ssl_functions.sh
+++ kthw/ca-certificate/ssl_functions.sh
@@ -13,7 +13,7 @@ log_error() {
     echo -e "\e[31;1m=== $1 \e[0m"
 }
 
-on_error() {
+on_failure() {
     if [ $? = 0 ]; then
         return
     fi
@@ -64,9 +64,76 @@ gen_csr() {
 
 }
 
+function sign_request() {
+	csr=$1
+	ca=$2
+	ca_key=$3
+	name=$4
+	ext_file=$5
+    extensions=$6
+
+	if [ ! -f "$ca" ] || [ ! -f "$ca_key" ] || [ ! -f "$csr" ] || [ -z "$name" ]; then
+		log_error "Usage: sign_request <csr> <ca_certificate> <ca_key> <certificate_name> [extension_file <extensions_section>] "
+		return
+	fi
+
+	if [ -n "$ext_file" ]; then
+		if [ ! -f $ext_file ] || [ -z "$extensions" ]; then
+			echo "extension file wasn't found or extensions section is not specified !"
+			return
+		fi
+
+		openssl x509 -req -days 365 -sha256 -in $csr -CA $ca -CAkey $ca_key \
+		-CAcreateserial -out $name -extfile $ext_file -extensions $extensions
+	else
+		openssl x509 -req -days 365 -sha256 -in $csr -CA $ca -CAkey $ca_key \
+		-CAcreateserial -out $name
+	fi
+}
+
 generate_admin_cert() {
     gen_key "admin.key"
     gen_csr "admin.key" "admin.csr" "admin.conf"
     on_error stop "gen_csr failed!"
 }
+
+
+generate_kubelet_client_certs() {
+############
+#  
+# generate_kubelet_client_certs
+#
+# DESCRIPTION: generates PKI for kubelet clients to communicate with the API server
+# 
+# USAGE:
+#   fill in the nodes, ips and hostnames arrays. 
+# 
+# EXPANATION:
+#   this will generate a private key and certificate for each node kubelet,
+#   with the subjectAltName field populated with IP and hostname.
+#
+###############
+    nodes=("node1" "node2")
+    ips=("1.2.3.4" "2.3.4.5")
+    hostnames=("node1.example.com" "node2.example.com")
+    if [ -d kubelets_certs ]; then
+        rm -rf kubelets_certs
+    fi
+
+    mkdir -p kubelet_certs/tmp; pushd kubelet_certs;
+    for node in ${nodes[@]}; do
+        mkdir $node;
+        sed "s/<node>/$node/;s/<hostname>/${hostnames[$node]}/;s/<ip>/${ips[$node]}/" ../kubelet.conf.template > "tmp/$node.conf"
+        gen_key "$node/kubelet.key"
+        gen_csr "$node/kubelet.key" "tmp/$node.csr" "tmp/$node.conf"
+        sign_request "tmp/$node.csr" ../ca.crt ../ca.key "$node/kubelet.crt" "tmp/$node.conf" "v3_ext"
+        on_failure stop "generate_kubelet_client_certs: failed generating for $node"
+        log_success "Generated PKI for $node"
+    done
+
+    rm -rf tmp
+    popd
+    log_success "SUCCESSFULLY generated PKI for kubelets!"
+}
+
 
