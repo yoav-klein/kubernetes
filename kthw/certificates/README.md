ad597ab844f2cfa65c40c16a6316337d2d5b824b
yoav <yoavklein25@gmail.com>
2022-07-03 22:11:59 +0000

    PKI

--- kthw/certificates/README.md
+++ kthw/certificates/README.md
@@ -1,17 +1,39 @@
 # Generate Certificates
 ---
 
+In this section, we're generating a bunch of TLS certificates to be used by the different parts
+of the kubernetes cluster.
+
+First, we generate a CA certificate, which will be used to sign all other certificates.
+
+## Client Certificates
+Overall, we generate the following client certificates:
+* admin
+* kube-controller-manager
+* A certificate for each kubelet
+* kube-proxy
+* kube-scheduler
+
+These certificates will be used by those components to authenticate to the API server.
+
+## Server Certificate
+Then, we generate the server certificate for the API server, to authenticate itself to the components.
+
 We'll use the `ssl_functions.sh` script to generate the certificates.
 Source this file to your shell session.
 
+## Service Account Key Pair
+Kubernetes uses a key pair to generate service account tokens.
+So we need to create a certificate for that also.
 
-## Generate CA certificate
-```
-$ generate_ca
-```
+## Usage
+---
 
-## Generate admin client certificate
-This is the certificate for the administrator
+In order to generate all the certificates, fill in the `machines.json` file with the information 
+about your worker nodes and control-plane nodes.
+
+Then, we have a Makefile that will do everything for us, utilizing the `pki.sh` script.
+
 ```
-$ generate_admin_cert
+$ make all
 ```
--- kthw/certificates/pki.sh
+++ kthw/certificates/pki.sh
@@ -34,7 +34,6 @@ on_failure() {
 }
 
 
-
 generate_admin_cert() {
     gen_private_key "admin.key"
     gen_sign_request "admin.key" "admin.csr" "admin.conf"
@@ -72,14 +71,28 @@ generate_kube_scheduler_cert() {
 }
 
 generate_kube_apiserver_cert() {
-    apiserver_ip="172.31.38.58"
+    cp kube-apiserver.conf.template kube-apiserver.conf
+    apiserver_ips=$(jq -r '."apiserver-ips"[]' $config_json)
+    i=0
+    for ip in $apiserver_ips; do
+        echo "IP.$i = $ip" >> kube-apiserver.conf
+        (( i = $i + 1 ))
+    done    
     gen_private_key "kube-apiserver.key"
-    sed "s/<ip>/$apiserver_ip/" kube-apiserver.conf.template > kube-apiserver.conf
     gen_sign_request "kube-apiserver.key" "kube-apiserver.csr" "kube-apiserver.conf"
     on_failure stop "Failed generating sign request for kube-apiserver"
     sign_request "kube-apiserver.csr" "ca.crt" "ca.key" "kube-apiserver.crt" "kube-apiserver.conf" "v3_ext"
+    rm "kube-apiserver.csr"
 }
 
+generate_service_accounts_cert() {
+    gen_private_key "service-accounts.key"
+    gen_sign_request "service-accounts.key" "service-accounts.csr" "service-accounts.conf"
+    on_failure stop "Failed generating sign_request for service-accounts"
+    sign_request "service-accounts.csr" "ca.crt" "ca.key" "service-accounts.crt"
+    on_failure stop "Generating certificate for service-accounts failed"
+    rm "service-accounts.csr"
+}
 
 generate_kubelet_client_certs() {
 ############
@@ -89,29 +102,32 @@ generate_kubelet_client_certs() {
 # DESCRIPTION: generates PKI for kubelet clients to communicate with the API server
 # 
 # USAGE:
-#   fill in the nodes, ips and hostnames arrays. 
+#   fill in the workers section in the configuration file with names, IP addresses and hostnames
 # 
-# EXPANATION:
+# EXPLANATION:
 #   this will generate a private key and certificate for each node kubelet,
 #   with the subjectAltName field populated with IP and hostname.
 #
 ###############
-    nodes=("node1" "node2")
-    ips=("1.2.3.4" "2.3.4.5")
-    hostnames=("node1.example.com" "node2.example.com")
+
+    workers=$(jq '.workers[]' $config_json -c)
     if [ -d kubelets_certs ]; then
         rm -rf kubelets_certs
     fi
 
     mkdir -p kubelet_certs/tmp; pushd kubelet_certs;
-    for node in ${nodes[@]}; do
-        mkdir $node;
-        sed "s/<node>/$node/;s/<hostname>/${hostnames[$node]}/;s/<ip>/${ips[$node]}/" ../kubelet.conf.template > "tmp/$node.conf"
-        gen_private_key "$node/kubelet.key"
-        gen_sign_request "$node/kubelet.key" "tmp/$node.csr" "tmp/$node.conf"
-        sign_request "tmp/$node.csr" ../ca.crt ../ca.key "$node/kubelet.crt" "tmp/$node.conf" "v3_ext"
-        on_failure stop "generate_kubelet_client_certs: failed generating for $node"
-        log_success "Generated PKI for $node"
+    for worker in $workers; do
+        node_name=$(echo $worker | jq -r '.name')
+        ip=$(echo $worker | jq -r '.ip')
+        hostname=$(echo $worker | jq -r '.hostname')
+        
+        mkdir $node_name
+        sed "s/<node>/$node_name/;s/<hostname>/$hostname/;s/<ip>/$ip/" ../kubelet.conf.template > "tmp/$node_name.conf"
+        gen_private_key "$node_name/kubelet.key"
+        gen_sign_request "$node_name/kubelet.key" "tmp/$node_name.csr" "tmp/$node_name.conf"
+        sign_request "tmp/$node_name.csr" ../ca.crt ../ca.key "$node_name/kubelet.crt" "tmp/$node_name.conf" "v3_ext"
+        on_failure stop "generate_kubelet_client_certs: failed generating for $node_name"
+        log_success "Generated PKI for $node_name"
     done
 
     rm -rf tmp
@@ -119,4 +135,8 @@ generate_kubelet_client_certs() {
     log_success "SUCCESSFULLY generated PKI for kubelets!"
 }
 
-source ssl/ssl_commons.sh
+
+config_json="machines.json"
+ssl_commons="ssl/ssl_commons.sh"
+
+source $ssl_commons
