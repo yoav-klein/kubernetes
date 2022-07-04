9f48d8c5b188dafe3d908b3878b394954f7fbf2d
yoav <yoavklein25@gmail.com>
2022-07-04 15:44:43 +0000

    PKI improved

--- kthw/certificates/pki.sh
+++ kthw/certificates/pki.sh
@@ -38,6 +38,7 @@ gen_certificate_generic() {
     ca_cert=$2
     ca_key=$3
     conf=$4
+    extensions=$5
     if [ -z "$name" ] || [ -z "$ca_cert" ] || [ -z "$ca_key" ]; then
         log_error "gen_certificate_regular: Usage: gen_certificate_regular <name> <ca_cert> <ca_key> [conf_file_path]"
         exit 1
@@ -51,44 +52,30 @@ gen_certificate_generic() {
     gen_private_key "$name.key"
     gen_sign_request "$name.key" "$name.csr" "$conf_file_path"
     on_failure stop "Failed generating sign request for $name !"
-    sign_request "$name.csr" $ca_cert $ca_key "$name.crt"
+
+    if [ -n "$extensions" ]; then 
+        sign_request "$name.csr" $ca_cert $ca_key "$name.crt" $conf_file_path $extensions
+    else 
+        sign_request "$name.csr" $ca_cert $ca_key "$name.crt"
+    fi
     on_failure stop "Generating certificate for $name failed !"
     rm "$name.csr"
 }
 
-generate_admin_cert() {
-    gen_certificate_generic "admin" $ca_cert $ca_key
-}
-
-generate_kube_ctrl_mgr_cert() {
-    gen_certificate_generic "kube-controller-manager" $ca_cert $ca_key
-}
-
-generate_kube_proxy_cert() {
-    gen_certificate_generic "kube-proxy" $ca_cert $ca_key
-}
-
-generate_kube_scheduler_cert() {
-    gen_certificate_generic "kube-scheduler" $ca_cert $ca_key
-}
-
-generate_kube_apiserver_cert() {
+patch_apiserver_config() {
+    template=$1
+    destination=$2
     if [ ! -d tmp ]; then mkdir tmp; fi
-    cp "$conf_files_base/kube-apiserver.conf.template" "tmp/kube-apiserver.conf"
+    cp $template $destination
     apiserver_ips=$(jq -r '."apiserver-ips"[]' $config_json)
     i=0
     for ip in $apiserver_ips; do
-        echo "IP.$i = $ip" >> kube-apiserver.conf
+        echo "IP.$i = $ip" >> $destination
         (( i = $i + 1 ))
-    done    
-    
-    gen_certificate_generic "kube-apiserver" $ca_cert $ca_key "tmp/kube-apiserver.conf"
+    done
     
 }
 
-generate_service_accounts_cert() {
-    gen_certificate_generic "service-accounts" $ca_cert $ca_key
-}
 
 
 generate_kubelet_client_certs() {
@@ -106,34 +93,27 @@ generate_kubelet_client_certs() {
 #   with the subjectAltName field populated with IP and hostname.
 #
 ###############
-
+    ca_cert=$1
+    ca_key=$2
+    conf_template=$3
+    destination=$4
     workers=$(jq '.workers[]' $config_json -c)
-    if [ -d kubelets_certs ]; then
-        rm -rf kubelets_certs
-    fi
-
-    mkdir -p kubelet_certs/tmp; pushd kubelet_certs;
+    if [ ! -d $destination ]; then mkdir -p $destination; fi
+    if [ ! -d tmp ]; then mkdir tmp; fi
     for worker in $workers; do
         node_name=$(echo $worker | jq -r '.name')
         ip=$(echo $worker | jq -r '.ip')
         hostname=$(echo $worker | jq -r '.hostname')
         
         mkdir $node_name
-        sed "s/<node>/$node_name/;s/<hostname>/$hostname/;s/<ip>/$ip/" ../kubelet.conf.template > "tmp/$node_name.conf"
-        gen_private_key "$node_name/kubelet.key"
-        gen_sign_request "$node_name/kubelet.key" "tmp/$node_name.csr" "tmp/$node_name.conf"
-        sign_request "tmp/$node_name.csr" ../ca.crt ../ca.key "$node_name/kubelet.crt" "tmp/$node_name.conf" "v3_ext"
-        on_failure stop "generate_kubelet_client_certs: failed generating for $node_name"
-        log_success "Generated PKI for $node_name"
+        sed "s/<node>/$node_name/;s/<hostname>/$hostname/;s/<ip>/$ip/" $conf_template > "tmp/$node_name.conf"
+        gen_certificate_generic $node_name/kubelet $ca_cert $ca_key "tmp/$node_name.conf" "v3_ext"
+        mv $node_name $destination
     done
-
     rm -rf tmp
-    popd
     log_success "SUCCESSFULLY generated PKI for kubelets!"
 }
 
-ca_cert="ca.crt"
-ca_key="ca.key"
 conf_files_base="cert-configs"
 config_json="machines.json"
 ssl_commons="ssl/ssl_commons.sh"
