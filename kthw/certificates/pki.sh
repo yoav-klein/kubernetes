024c81463f8ad42d377dc0e169b4303da8baa2fd
yoav <yoavklein25@gmail.com>
2022-07-04 10:41:35 +0000

    rm ca-certificate

--- kthw/certificates/pki.sh
+++ kthw/certificates/pki.sh
@@ -33,66 +33,63 @@ on_failure() {
         
 }
 
+gen_certificate_generic() {
+    name=$1
+    ca_cert=$2
+    ca_key=$3
+    conf=$4
+    if [ -z "$name" ] || [ -z "$ca_cert" ] || [ -z "$ca_key" ]; then
+        log_error "gen_certificate_regular: Usage: gen_certificate_regular <name> <ca_cert> <ca_key> [conf_file_path]"
+        exit 1
+    fi
+
+    if [ -n "$conf" ]; then
+        conf_file_path=$4
+    else
+        conf_file_path="$conf_files_base/$name.conf"
+    fi
+    gen_private_key "$name.key"
+    gen_sign_request "$name.key" "$name.csr" "$conf_file_path"
+    on_failure stop "Failed generating sign request for $name !"
+    sign_request "$name.csr" $ca_cert $ca_key "$name.crt"
+    on_failure stop "Generating certificate for $name failed !"
+    rm "$name.csr"
+}
 
 generate_admin_cert() {
-    gen_private_key "admin.key"
-    gen_sign_request "admin.key" "admin.csr" "admin.conf"
-    on_failure stop "Failed generating sign request for admin"
-    sign_request "admin.csr" "ca.crt" "ca.key" "admin.crt"
-    on_failure stop "Generating certificate for admin failed"
-    rm "admin.csr"
+    gen_certificate_generic "admin" $ca_cert $ca_key
 }
 
 generate_kube_ctrl_mgr_cert() {
-    gen_private_key "kube-controller-manager.key"
-    gen_sign_request "kube-controller-manager.key" "kube-controller-manager.csr" "kube-controller-manager.conf"
-    on_failure stop "Failed generating sign request for kube-controller-manager"
-    sign_request "kube-controller-manager.csr" "ca.crt" "ca.key" "kube-controller-manager.crt"
-    on_failure stop "Generating certificate for kube-controller-manager failed"
-    rm "kube-controller-manager.csr"
+    gen_certificate_generic "kube-controller-manager" $ca_cert $ca_key
 }
 
 generate_kube_proxy_cert() {
-    gen_private_key "kube-proxy.key"
-    gen_sign_request "kube-proxy.key" "kube-proxy.csr" "kube-proxy.conf"
-    on_failure stop "Failed generating sign request for kube-proxy"
-    sign_request "kube-proxy.csr" "ca.crt" "ca.key" "kube-proxy.crt"
-    on_failure stop "Generating certificate for kube-proxy failed"
-    rm "kube-proxy.csr"
+    gen_certificate_generic "kube-proxy" $ca_cert $ca_key
 }
 
 generate_kube_scheduler_cert() {
-    gen_private_key "kube-scheduler.key"
-    gen_sign_request "kube-scheduler.key" "kube-scheduler.csr" "kube-scheduler.conf"
-    on_failure stop "Failed generating sign request for kube-scheduler"
-    sign_request "kube-scheduler.csr" "ca.crt" "ca.key" "kube-scheduler.crt"
-    on_failure stop "Generating certificate for kube-scheduler failed"
-    rm "kube-scheduler.csr"
+    gen_certificate_generic "kube-scheduler" $ca_cert $ca_key
 }
 
 generate_kube_apiserver_cert() {
-    cp kube-apiserver.conf.template kube-apiserver.conf
+    if [ ! -d tmp ]; then mkdir tmp; fi
+    cp "$conf_files_base/kube-apiserver.conf.template" "tmp/kube-apiserver.conf"
     apiserver_ips=$(jq -r '."apiserver-ips"[]' $config_json)
     i=0
     for ip in $apiserver_ips; do
         echo "IP.$i = $ip" >> kube-apiserver.conf
         (( i = $i + 1 ))
     done    
-    gen_private_key "kube-apiserver.key"
-    gen_sign_request "kube-apiserver.key" "kube-apiserver.csr" "kube-apiserver.conf"
-    on_failure stop "Failed generating sign request for kube-apiserver"
-    sign_request "kube-apiserver.csr" "ca.crt" "ca.key" "kube-apiserver.crt" "kube-apiserver.conf" "v3_ext"
-    rm "kube-apiserver.csr"
+    
+    gen_certificate_generic "kube-apiserver" $ca_cert $ca_key "tmp/kube-apiserver.conf"
+    
 }
 
 generate_service_accounts_cert() {
-    gen_private_key "service-accounts.key"
-    gen_sign_request "service-accounts.key" "service-accounts.csr" "service-accounts.conf"
-    on_failure stop "Failed generating sign_request for service-accounts"
-    sign_request "service-accounts.csr" "ca.crt" "ca.key" "service-accounts.crt"
-    on_failure stop "Generating certificate for service-accounts failed"
-    rm "service-accounts.csr"
+    gen_certificate_generic "service-accounts" $ca_cert $ca_key
 }
+
 
 generate_kubelet_client_certs() {
 ############
@@ -135,7 +132,9 @@ generate_kubelet_client_certs() {
     log_success "SUCCESSFULLY generated PKI for kubelets!"
 }
 
-
+ca_cert="ca.crt"
+ca_key="ca.key"
+conf_files_base="cert-configs"
 config_json="machines.json"
 ssl_commons="ssl/ssl_commons.sh"
 
