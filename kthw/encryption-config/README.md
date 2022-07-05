603207d19007da69d2aded6134ed741b9a33ff25
yoav <yoavklein25@gmail.com>
2022-07-05 20:45:43 +0000

    Encryption config

---
+++ kthw/encryption-config/README.md
@@ -0,0 +1,13 @@
+# Data Encryption Config
+---
+
+Kubernetes encrypts data at rest. For this, it needs an encryption key.
+
+We need to generate this encryption key, put it in a configuration file and distribute it
+to our control-plane nodes.
+
+## Usage
+
+```
+$ make
+```
---
+++ kthw/encryption-config/utils.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+generate_file() {
+    SECRET=$(head -c 32 /dev/urandom | base64)
+    
+    cat > encryption-config.yaml <<- EOF
+    apiVersion: v1
+    kind: EncryptionConfig
+    resources:
+    - resources:
+       - secrets
+      providers:
+        - aescbc:
+            keys:
+            - name: key1
+                secret: $SECRET
+        - identity: {}
+EOF
+    
+}
