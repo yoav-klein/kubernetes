ee7ab041c80888545a8f3194005ab2552b4e5b00
yoav <yoavklein25@gmail.com>
2022-07-14 14:14:25 +0000

    KTHW:: merge master to etcd

--- kthw/configure.sh
+++ kthw/configure.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+
+## Install kubectl
 kubectl_version="v1.24.3"
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/${kubectl_version}/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
-
 if ! ./kubectl 1>/dev/null; then echo "kubectl failed !!"; exit 1; fi
-
 sudo mv ./kubectl /usr/local/bin
 
+## Genearate a .env file 
 sed  "s@{{pwd}}@$(pwd)@" env > .env
+
+## Configure vim for convinience
 cp -r vim/.vim vim/.vimrc ~
 sudo apt-get update
 sudo apt-get install -y jq make
