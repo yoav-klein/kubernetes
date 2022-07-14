b88e2402e2917dadb2e057c7501ebc94913e1f9f
yoav <yoavklein25@gmail.com>
2022-07-14 11:21:34 +0000

    KTHW::etcd:: start

--- kthw/etcd/run.sh
+++ kthw/etcd/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+source ../.env
+
 sudo mkdir -p /etc/etcd /var/lib/etcd
+
+sudo cp $CERTIFICATES_OUTPUT/ca.crt \
+    $CERTIFICATES_OUTPUT/kube-apiserver.crt \
+    $CERTIFICATES_OUTPUT/kube-apiserver.key  \
+    /etc/etcd
+
+
