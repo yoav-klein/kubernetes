ca3bd79e5e06826b031885cf206a872e08728cb8
yoav <yoavklein25@gmail.com>
2022-07-14 10:57:06 +0000

    KTHW:: - Adding kubeconfig values to env file - Installing kubectl in configure.sh - Editing Makefile of kubeconfigs

--- kthw/configure.sh
+++ kthw/configure.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+kubectl_version="v1.24.3"
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/${kubectl_version}/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+
+if ! ./kubectl 1>/dev/null; then echo "kubectl failed !!"; exit 1; fi
+
+sudo mv ./kubectl /usr/local/bin
+
 sed  "s@{{pwd}}@$(pwd)@" env > .env
 cp -r vim/.vim vim/.vimrc ~
 sudo apt-get update
