00a6e448f52ffe334983c28ca4c65ed517e59608
yoav <yoavklein25@gmail.com>
2022-07-16 22:29:41 +0000

    KTHW::ETCD:: going in

--- kthw/etcd/copy_files_to_controllers.sh
+++ kthw/etcd/copy_files_to_controllers.sh
@@ -5,16 +5,32 @@ source ../.env
 username=$(jq -r ".machinesUsername" $ROOT_DATA_FILE)
 controllers=($(jq -c ".controllers[]" $ROOT_DATA_FILE))
 
-for controller in ${controllers[@]}; do
-    ip=$(echo $controller | jq -r ".ip")
-    name=$(echo $controller | jq -r ".name")
-    ssh -i $SSH_PRIVATE_KEY "$username@$ip" "mkdir -p ~/k8s/etcd"
 
-    scp -i $SSH_PRIVATE_KEY "$name.etcd.service" "$username@$ip:~/k8s/etcd/"
-    scp -i $SSH_PRIVATE_KEY $CERTIFICATES_OUTPUT/ca.crt "$username@$ip:~/k8s/etcd/"
-    scp -i $SSH_PRIVATE_KEY $CERTIFICATES_OUTPUT/kube-apiserver.crt "$username@$ip:~/k8s/etcd/"
-    scp -i $SSH_PRIVATE_KEY $CERTIFICATES_OUTPUT/kube-apiserver.key "$username@$ip:~/k8s/etcd/"
-done
+copy_files_to_controllers() {
+    for controller in ${controllers[@]}; do
+        ip=$(echo $controller | jq -r ".ip")
+        name=$(echo $controller | jq -r ".name")
+        
+        echo "ssh -i $SSH_PRIVATE_KEY \"$username@$ip\" \"mkdir -p ~/k8s/etcd\""
+        ssh -i $SSH_PRIVATE_KEY "$username@$ip" "mkdir -p ~/k8s/etcd"
+       
+        scp -i $SSH_PRIVATE_KEY "$name.etcd.service" "$username@$ip:~/k8s/etcd/etcd.service"
+        scp -i $SSH_PRIVATE_KEY $CERTIFICATES_OUTPUT/ca.crt "$username@$ip:~/k8s/etcd/"
+        scp -i $SSH_PRIVATE_KEY $CERTIFICATES_OUTPUT/kube-apiserver.crt "$username@$ip:~/k8s/etcd/"
+        scp -i $SSH_PRIVATE_KEY $CERTIFICATES_OUTPUT/kube-apiserver.key "$username@$ip:~/k8s/etcd/"
+        scp -i $SSH_PRIVATE_KEY ./setup.sh "$username@$ip:~/k8s/etcd"
+    done
+}
+
+run_setup() {
+    for controller in ${controllers[@]}; do
+        ip=$(echo $controller | jq -r ".ip")
+        ssh -i $SSH_PRIVATE_KEY "$username@$ip" ~/k8s/etcd/setup.sh
+    done
+}
+
+copy_files_to_controllers
+run_setup
 
 # foreach controller node:
 # 1. scp unit file
--- kthw/etcd/prepare_files.sh
+++ kthw/etcd/prepare_files.sh
@@ -5,12 +5,13 @@ source ../.env
 
 function create_initial_cluster_var() {
     server_ips=($(jq -r ".controllers[].ip" $ROOT_DATA_FILE))
+    controllers_names=($(jq -r ".controllers[].name" $ROOT_DATA_FILE))
 
     initial_cluster=""
     num_controllers=${#server_ips[@]}
 
     for i in $(seq 0 $(( $num_controllers - 1 )) ); do
-        initial_cluster="${initial_cluster}${server_ips[i]}:2380"
+        initial_cluster="${initial_cluster}${controllers_names[i]}=https://${server_ips[i]}:2380"
         if ! (( $num_controllers == $(( i+1 )) )) ; then initial_cluster="${initial_cluster},"; fi 
     done
 }
@@ -27,10 +28,17 @@ function generate_files() {
         
         sed "s/{{ETCD_NAME}}/$name/" $template  \
             | sed "s/{{INTERNAL_IP}}/$ip/" \
-            | sed "s/{{INITIAL_CLUSTER}}/$initial_cluster/" - > $name.etcd.service
+            | sed "s@{{INITIAL_CLUSTER}}@$initial_cluster@" - > $name.etcd.service
        
     done
 }
 
+patch_setup_script() {
+    etcd_version=$(jq -r ".versions.etcd" $ROOT_DATA_FILE)
+    sed "s/{{etcd_version}}/$etcd_version/" setup.sh.template > setup.sh
+    chmod +x setup.sh
+}
+
 create_initial_cluster_var
 generate_files
+#patch_setup_script
