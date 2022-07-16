4ebdf0c821653a6729fa1a6aadcbbbf306ce63c0
yoav <yoavklein25@gmail.com>
2022-07-16 20:59:19 +0000

    KTHW::ETCD::Copy files to controllers:: copying using SCP

--- kthw/etcd/copy_files_to_controllers.sh
+++ kthw/etcd/copy_files_to_controllers.sh
@@ -2,11 +2,18 @@
 
 source ../.env
 
+username=$(jq -r ".machinesUsername" $ROOT_DATA_FILE)
 controllers=($(jq -c ".controllers[]" $ROOT_DATA_FILE))
-echo $controllers
 
 for controller in ${controllers[@]}; do
-    echo $controller
+    ip=$(echo $controller | jq -r ".ip")
+    name=$(echo $controller | jq -r ".name")
+    ssh -i $SSH_PRIVATE_KEY "$username@$ip" "mkdir -p ~/k8s/etcd"
+
+    scp -i $SSH_PRIVATE_KEY "$name.etcd.service" "$username@$ip:~/k8s/etcd/"
+    scp -i $SSH_PRIVATE_KEY $CERTIFICATES_OUTPUT/ca.crt "$username@$ip:~/k8s/etcd/"
+    scp -i $SSH_PRIVATE_KEY $CERTIFICATES_OUTPUT/kube-apiserver.crt "$username@$ip:~/k8s/etcd/"
+    scp -i $SSH_PRIVATE_KEY $CERTIFICATES_OUTPUT/kube-apiserver.key "$username@$ip:~/k8s/etcd/"
 done
 
 # foreach controller node:
