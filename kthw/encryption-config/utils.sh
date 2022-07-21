50c0ead13b8c108957d15ad87a1b1aac10fb2f0b
Ubuntu <yoavklein25@gmail.com>
2022-07-21 09:05:25 +0000

    KTHW:: Fix encryption-config.yaml

--- kthw/encryption-config/utils.sh
+++ kthw/encryption-config/utils.sh
@@ -4,17 +4,17 @@ generate_file() {
     SECRET=$(head -c 32 /dev/urandom | base64)
     
     cat > encryption-config.yaml <<- EOF
-    apiVersion: v1
-    kind: EncryptionConfig
-    resources:
-    - resources:
-       - secrets
-      providers:
-        - aescbc:
-            keys:
-            - name: key1
-                secret: $SECRET
-        - identity: {}
+apiVersion: v1
+kind: EncryptionConfig
+resources:
+- resources:
+  - secrets
+providers:
+- aescbc:
+    keys:
+    - name: key1
+      secret: $SECRET
+- identity: {}
 EOF
     
 }
