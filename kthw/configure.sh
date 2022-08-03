d3f924d1ac9cff815994d85d2e7fd1ced409b1b8
Ubuntu <yoav@controller1.kqgnbyrg5piudkhlwv15bdycoh.frax.internal.cloudapp.net>
2022-08-03 22:04:13 +0000

    KTHW:: configure script fix

--- kthw/configure.sh
+++ kthw/configure.sh
@@ -5,17 +5,23 @@ __configure_sourced__=1
 [ -f "./kthw_key" ] || { echo "Create a kthw_key file with the private key to access all machines"; return 1 ; }
 chmod 600 ./kthw_key
 
-## Install kubectl
-kubectl_version="v1.24.3"
-# Install kubectl
-curl -LO "https://dl.k8s.io/release/${kubectl_version}/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
-if ! ./kubectl 1>/dev/null; then echo "kubectl failed !!"; exit 1; fi
-sudo mv ./kubectl /usr/local/bin
+## Install required tools
+sudo apt-get update
+sudo apt-get install -y jq make
+
 
 ## Genearate a .env file 
 sed  "s@{{pwd}}@$(pwd)@" env > .env
 source .env
+
+## Install kubectl
+echo $ROOT_DATA_FILE
+kubectl_version=$(jq -r '.versions.kubernetes' $ROOT_DATA_FILE)
+echo $kubectl_version
+curl -LO "https://dl.k8s.io/release/v${kubectl_version}/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+if ! ./kubectl 1>/dev/null; then echo "kubectl failed !!"; return 1; fi
+sudo mv ./kubectl /usr/local/bin
 
 ## Disable SSH strict host key checking
 #sudo sed -i -E 's/#[[:space:]]*(StrictHostKeyChecking[[:space:]]*)ask/\1no/' /etc/ssh/ssh_config
@@ -23,10 +29,6 @@ echo "StrictHostKeyChecking no" > ~/.ssh/config
 
 # clone logging library
 git clone https://github.com/yoav-klein/bash
-
-## Install required tools
-sudo apt-get update
-sudo apt-get install -y jq make
 
 # set hte KUBECONFIG environment variable
 export KUBECONFIG=${KUBECONFIGS_OUTPUT}/admin.kubeconfig
